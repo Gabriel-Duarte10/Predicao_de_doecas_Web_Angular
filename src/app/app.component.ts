dce6667f4d0e16311807f730668f83f22de3bac4
Gabriel Duarte <gabriel.duarte@lojainterativa.com>
2022-04-23 02:36:28 -0300

    trocando input para select feito com sucesso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'PredicaoDoencas';
+export class AppComponent implements OnInit {
+  myForm!: FormGroup
+
+  get colors(): FormArray {
+    return this.myForm.get('colors') as FormArray
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      colors: this.fb.array([
+        this.fb.group({name: 'red'}),
+        this.fb.group({name: 'green'}),
+        this.fb.group({name: 'blue'}),
+      ]),
+    })
+  }
+
+  removeColor(index: number) {
+    this.colors.removeAt(index)
+  }
+
+  addColor() {
+    this.colors.push(this.fb.group({name: ''}))
+  }
+
 }
